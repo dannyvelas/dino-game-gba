24d3bbe328f21fb1635d2068f610d623835d9bdf
Daniel Velasquez <danivelas4@gmail.com>
2024-12-27 15:06:13 -0500

    able to render one sprite from a sprite sheet with two sprites in 1d mode A graphics/character.grit A graphics/character.png M source/main.c

--- source/main.c
+++ source/main.c
@@ -1,33 +1,38 @@
 #include <string.h>
 #include <tonc.h>
-#include "dino.h"
+#include "character.h"
 
 int main() {
-    OBJ_ATTR *dino = &oam_mem[0];
+    OBJ_ATTR *character = &oam_mem[0];
     
-    // Place the glyphs of a 4bpp boxed dino sprite
-    memcpy32(&tile_mem[4][0], dinoTiles, dinoTilesLen / sizeof(u32));
-    memcpy16(pal_obj_mem, dinoPal, dinoPalLen / sizeof(u16));
+    // Place the glyphs of a 4bpp boxed character sprite
+    memcpy32(&tile_mem[4][0], characterTiles, characterTilesLen / sizeof(u32));
+    memcpy16(pal_obj_mem, characterPal, characterPalLen / sizeof(u16));
 
-    // init OAM for this dino
-    oam_init(dino, 1);
+    // init OAM for this character
+    oam_init(character, 1);
     REG_DISPCNT= DCNT_OBJ | DCNT_OBJ_1D;
 
-    int x= 12, y= 96;
+    int x= 96, y= 32;
     u32 tid= 0, pb= 0;        // tile id, pal-bank
 
-    obj_set_attr(dino,
+    obj_set_attr(character,
       ATTR0_SQUARE, // Square, regular sprite
       ATTR1_SIZE_32, // 32x32p
       ATTR2_PALBANK(pb) | tid);   // palbank 0, tile 0
-    obj_set_pos(dino, x, y);
+    obj_set_pos(character, x, y);
 
     while(1) {
         vid_vsync();
         key_poll();
  
-        pb= key_is_down(KEY_SELECT) ? 1 : 0;
-        dino->attr2= ATTR2_BUILD(tid, pb, 0);
+        if (key_hit(KEY_L)) {
+          tid -= 32;
+        } else if (key_hit(KEY_R)) {
+          tid += 32;
+        }
+        
+        character->attr2= ATTR2_BUILD(tid, pb, 0);
     }
     
     return 0;
