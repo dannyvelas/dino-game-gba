bb441526c14fd657149a3dec2fdc065c13e4b2d5
Daniel Velasquez <danivelas4@gmail.com>
2024-12-31 18:39:09 -0500

    re-ordered if stmts M source/main.c

--- source/main.c
+++ source/main.c
@@ -67,21 +67,21 @@ int main() {
       continue;
     }
 
-    if ((direction == -1 && -4 <= offset && offset < 0) ||
-        (direction == 1 && -5 <= offset && offset < 0)) {
-      // if we're in the middle of going up or going down in a jump
-      // continue moving in that direction
-      offset += 1 * direction;
-      y += TILE_HEIGHT * direction;
-      obj_set_pos(&dino, x, y);
-      oam_copy(oam_mem, &dino, 1);
-    } else if (direction == -1 && offset == -5) {
+    if (direction == -1 && offset == -5) {
       // if we reached the arc of our jump, start going down
       direction = 1;
     } else if (offset == 0 && direction == 1) {
       // if we reached the floor after a jump, flip the direction
       // so that when we jump we go up again
       direction = -1;
+    } else if ((direction == -1 && -4 <= offset && offset < 0) ||
+               (direction == 1 && -5 <= offset && offset < 0)) {
+      // if we're in the middle of going up or going down in a jump
+      // continue moving in that direction
+      offset += 1 * direction;
+      y += TILE_HEIGHT * direction;
+      obj_set_pos(&dino, x, y);
+      oam_copy(oam_mem, &dino, 1);
     }
   }
 
