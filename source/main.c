f5d46825e52c1fb11e30d56ad946dc2ca26669d6
Daniel Velasquez <danivelas4@gmail.com>
2025-01-17 15:35:25 -0500

    made footwork more similar to the game M graphics/dino.grit M graphics/dino.png M source/main.c

--- source/main.c
+++ source/main.c
@@ -11,19 +11,21 @@
 #define AMT_ROWS (SCREEN_HEIGHT / TILE_HEIGHT)
 #define TILE_N 32
 
+enum dino_action { JUMPING, LEFT_STEP, RIGHT_STEP };
+
 struct state {
   /* oam_init requires that this field is big enough for for one OBJ_AFFINE */
   OBJ_ATTR dino[4];
   int x;
   int y;
   int direction;
-  u8 jump_initiated;
+  enum dino_action action;
 };
 
 void jump(struct state *dino_state, int start_y) {
   int offset = dino_state->y - start_y;
-  int jump_speed = TILE_HEIGHT/2;
-  int arc = -(jump_speed * 10);
+  int jump_speed = TILE_HEIGHT / 2;
+  int arc = -(jump_speed * 20);
   if (dino_state->direction == -1 && offset == arc) {
     // if we reached the arc of our jump, start going down
     dino_state->direction = 1;
@@ -31,7 +33,7 @@ void jump(struct state *dino_state, int start_y) {
     // if we reached the floor after a jump, flip the direction
     // so that when we jump we go up again
     dino_state->direction = -1;
-    dino_state->jump_initiated = 0;
+    dino_state->action = LEFT_STEP;
   } else if ((dino_state->direction == -1 && offset <= 0) ||
              (dino_state->direction == 1 && offset <= -jump_speed)) {
     // if we're in the middle of going up or going down in a jump
@@ -55,9 +57,8 @@ int main() {
   memcpy32(pal_bg_mem, skyPal, skyPalLen / sizeof(u32));
   memcpy16(&tile_mem[CBB_INDEX][0], skyTiles, skyTilesLen / sizeof(u16));
 
-  // load background tile maps
-  int floor_tile_y =
-      (AMT_ROWS - 4); // lets make the floor 3 tiles up from bottom
+  // load background tile maps. lets make the floor 3 tiles up from bottom
+  int floor_tile_y = (AMT_ROWS - 4);
   int floor_tile_index = 1;
   toncset16(&se_mem[SBB_INDEX][floor_tile_y * TILE_N], floor_tile_index,
             TILE_N);
@@ -75,7 +76,7 @@ int main() {
       .x = start_x,
       .y = start_y,
       .direction = -1,
-      .jump_initiated = 0,
+      .action = LEFT_STEP,
   };
   // init default object values for our dino. we will copy this to OAM on VBLANK
   oam_init(dino_state.dino, 1);
@@ -83,27 +84,34 @@ int main() {
   obj_set_attr(dino_state.dino, ATTR0_SQUARE, ATTR1_SIZE_32,
                ATTR2_PALBANK(palette_bank) | tile_index);
 
-  u16 h_scroll = 0;
+  u16 i = 0;
   while (1) {
     vid_vsync();
     key_poll();
     if (dino_state.y == start_y && key_hit(KEY_A)) {
       // if our y coordinate is at start and A is hit, start a jump
-      dino_state.jump_initiated = 1;
+      dino_state.action = JUMPING;
       continue;
     }
 
-    if (dino_state.jump_initiated) {
+    if (dino_state.action == JUMPING) {
       jump(&dino_state, start_y);
+    } else if (i % 4 == 0 && dino_state.action == LEFT_STEP) {
+      dino_state.action = RIGHT_STEP;
+    } else if (i % 4 == 0 && dino_state.action == RIGHT_STEP) {
+      dino_state.action = LEFT_STEP;
     }
+
+    obj_set_attr(dino_state.dino, ATTR0_SQUARE, ATTR1_SIZE_32,
+                 ATTR2_PALBANK(palette_bank) | (dino_state.action * 16));
 
     // update OAM with new values that were calculated in this frame
     obj_set_pos(dino_state.dino, dino_state.x, dino_state.y);
     oam_copy(oam_mem, dino_state.dino, 1);
 
     // scroll horizontal window
-    REG_BG0HOFS = h_scroll;
-    h_scroll += 1;
+    REG_BG0HOFS = i;
+    i += 1;
   }
 
   return 0;
