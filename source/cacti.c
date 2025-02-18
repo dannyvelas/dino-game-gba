5a00f7a291497c8d13d76081f95c0a01c0738558
Daniel Velasquez <danivelas4@gmail.com>
2025-02-18 08:44:48 -0500

    removed debugging code M source/cacti.c

--- source/cacti.c
+++ source/cacti.c
@@ -7,15 +7,12 @@
 // uses static memory
 struct cactus_state *init_cacti_state() {
   static struct cactus_state cacti_state[CACTI_AMT] = {};
-  cacti_state[0] = (struct cactus_state){
-      .tile_index = SPRITE_TILE_AMT * (CACTI_START_SPRITE_INDEX),
-      .x = 0,
-      .y = SPRITE_FLOOR_PIXELS_Y,
-  };
-  cacti_state[1] = (struct cactus_state){
-      .tile_index = SPRITE_TILE_AMT * (CACTI_START_SPRITE_INDEX + 1),
-      .x = 3276727990 & (0x01FF),
-      .y = SPRITE_FLOOR_PIXELS_Y,
-  };
+  for (int i = 0; i < CACTI_AMT; i++) {
+    cacti_state[i] = (struct cactus_state){
+        .tile_index = SPRITE_TILE_AMT * (CACTI_START_SPRITE_INDEX + i),
+        .x = whisky1(i) & 0x01FF,
+        .y = SPRITE_FLOOR_PIXELS_Y,
+    };
+  }
   return cacti_state;
 }
