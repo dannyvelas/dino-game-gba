f803292a3fd9d54236643d1a430cef4996fce90e
Daniel Velasquez <danivelas4@gmail.com>
2025-02-21 22:17:37 -0500

    extracted collision detection to a different func M source/main.c

--- source/main.c
+++ source/main.c
@@ -13,6 +13,33 @@ int cactus_left_lt_dino_right(int cactus_left, int cactus_right,
   return cactus_left < dino_right;
 }
 
+int detect_collision(struct dino_state dino_state,
+                     struct cactus_state *cacti_state) {
+  for (int i = 0; i < CACTI__AMT; i++) {
+    // check if dino hit me
+    for (int j = 0; j < DINO__HITBOX_AMT; j++) {
+      int dino_left = dino_state.hitboxes[j].left;
+      int dino_right = dino_state.hitboxes[j].right;
+      int dino_bottom = dino_state.y + dino_state.hitboxes[j].bottom;
+
+      for (int k = 0; k < CACTI__HITBOX_AMT; k++) {
+        int cactus_left =
+            (cacti_state[i].x + cacti_state[i].hitboxes[k].left) & 0x01FF;
+        int cactus_top = cacti_state[i].y + cacti_state[i].hitboxes[k].top;
+        int cactus_right =
+            (cacti_state[i].x + cacti_state[i].hitboxes[k].right) & 0x01FF;
+
+        if ((cactus_left_lt_dino_right(cactus_left, cactus_right,
+                                       dino_right)) &&
+            (cactus_right >= dino_left) && (dino_bottom >= cactus_top)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
 int main() {
   // set I/O register to use mode0, sprites, 1d sprites and tiled background 0
   REG_DISPCNT = DCNT_MODE0 | DCNT_OBJ | DCNT_OBJ_1D | DCNT_BG0;
@@ -55,27 +82,12 @@ int main() {
       cacti_state[i].x = (cacti_state[i].x - scroll_velocity) & 0x01FF;
       obj_set_pos(cacti_state[i].cactus_obj, cacti_state[i].x,
                   cacti_state[i].y);
+    }
 
-      // check if dino hit me
-      for (int j = 0; j < DINO__HITBOX_AMT; j++) {
-        int dino_left = dino_state.hitboxes[j].left;
-        int dino_right = dino_state.hitboxes[j].right;
-        int dino_bottom = dino_state.y + dino_state.hitboxes[j].bottom;
-
-        for (int k = 0; k < CACTI__HITBOX_AMT; k++) {
-          int cactus_left =
-              (cacti_state[i].x + cacti_state[i].hitboxes[k].left) & 0x01FF;
-          int cactus_top = cacti_state[i].y + cacti_state[i].hitboxes[k].top;
-          int cactus_right =
-              (cacti_state[i].x + cacti_state[i].hitboxes[k].right) & 0x01FF;
-
-          if ((cactus_left_lt_dino_right(cactus_left, cactus_right,
-                                         dino_right)) &&
-              (cactus_right >= dino_left) && (dino_bottom >= cactus_top)) {
-            dino_state.alive = 0;
-          }
-        }
-      }
+    // detect if collision happened after everything has been moved
+    int collision_happened = detect_collision(dino_state, cacti_state);
+    if (collision_happened) {
+      dino_state.alive = 0;
     }
 
     // scroll horizontal window
