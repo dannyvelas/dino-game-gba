0719ee8ca5c3866c1da5716258a6f79550cbb0e4
Daniel Velasquez <danivelas4@gmail.com>
2025-03-30 19:39:44 -0400

    got text M source/main.c M source/world.c

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 #include <tonc.h>
 
 int main() {
-  // set I/O register to use mode0, sprites, 1d sprites and tiled background 0
+  // set I/O reg to use mode0, sprites, 1d sprites and tiled backgrounds 0 & 1
   REG_DISPCNT = DCNT_MODE0 | DCNT_OBJ | DCNT_OBJ_1D | DCNT_BG0;
   // put tonc interrupt switchboard at address that is used for interrupts
   irq_init(NULL);
@@ -17,46 +17,46 @@ int main() {
   // load background and sprite assets
   load_world();
   // also initialize the background on the screen
-  init_world();
+  // init_world();
 
   // initialize our object buffer that will be used for the rest of this program
-  struct buffer_state buffer_state = init_buffer();
-  // initialize dino state with a pointer to an object buffer
-  struct dino_state dino_state = init_dino_state(&buffer_state);
-  // init cacti
-  struct cactus_state *cacti_state = init_cacti_state(&buffer_state);
+  // struct buffer_state buffer_state = init_buffer();
+  //// initialize dino state with a pointer to an object buffer
+  // struct dino_state dino_state = init_dino_state(&buffer_state);
+  //// init cacti
+  // struct cactus_state *cacti_state = init_cacti_state(&buffer_state);
 
-  int frame = 0;
-  int scroll_velocity = 2;
-  int scroll_offset = 0;
-  while (1) {
-    VBlankIntrWait();
-    key_poll();
+  // int frame = 0;
+  // int scroll_velocity = 2;
+  // int scroll_offset = 0;
+  // while (1) {
+  //   VBlankIntrWait();
+  //   key_poll();
 
-    // update OAM with new values that were calculated in last frame
-    oam_copy(oam_mem, buffer_state.obj_buffer, buffer_state.len);
-    // if out dinosaur crashed into something last iteration, lets break
-    if (!dino_state.alive) {
-      break;
-    }
+  //  // update OAM with new values that were calculated in last frame
+  //  oam_copy(oam_mem, buffer_state.obj_buffer, buffer_state.len);
+  //  // if out dinosaur crashed into something last iteration, lets break
+  //  if (!dino_state.alive) {
+  //    break;
+  //  }
 
-    // update dino state struct, and dino buffer
-    update_dino_state(&dino_state, frame);
+  //  // update dino state struct, and dino buffer
+  //  update_dino_state(&dino_state, frame);
 
-    // update cacti state struct, and cacti buffers
-    update_cacti_state(cacti_state, scroll_velocity);
+  //  // update cacti state struct, and cacti buffers
+  //  update_cacti_state(cacti_state, scroll_velocity);
 
-    // if collision happened, update dino state to be gameover
-    if (detected_collision(dino_state, cacti_state)) {
-      dino_state.alive = 0;
-      update_dino_state(&dino_state, frame);
-    }
+  //  // if collision happened, update dino state to be gameover
+  //  if (detected_collision(dino_state, cacti_state)) {
+  //    dino_state.alive = 0;
+  //    update_dino_state(&dino_state, frame);
+  //  }
 
-    // scroll horizontal window
-    scroll_offset += scroll_velocity;
-    REG_BG0HOFS = scroll_offset;
-    frame += 1;
-  }
+  //  // scroll horizontal window
+  //  scroll_offset += scroll_velocity;
+  //  REG_BG0HOFS = scroll_offset;
+  //  frame += 1;
+  //}
 
   // game over in low power mode
   while (1) {
--- source/world.c
+++ source/world.c
@@ -3,35 +3,68 @@
 #include "sprites.h"
 #include "util.h"
 
-static const int CBB_INDEX = 0;
-// convention is to make the screen base block as far right in VRAM as you can
-// we're using a map of size 32x32 tiles, so we can use the very last one
-static const int SBB_INDEX = 31;
+// we'll use CBB #1 for generic background tiles in the deepest background (BG0)
+// and CBB #0 for text, one background above (BG1)
+static const int BG0_CBB = 1;
+static const int BG1_CBB = 0;
 
-void load_world() {
-  // set the background using charblock 0 as the character base block
-  // and SBB. each background tile is 4bpp. background is of size 32x32 tiles.
-  REG_BG0CNT = BG_CBB(CBB_INDEX) | BG_SBB(SBB_INDEX) | BG_4BPP | BG_REG_32x32;
+// we're using a map of size 32x32 screen-entries, (aka 32*32*2 => 2048 bytes)
+// which is the same exact size as one screenblock entry
+// convention is to make the screen base block as far right in VRAM as you can,
+// so we'll use the last one
+static const int BG0_SBB = 31;
+static const int BG1_SBB = 30;
+
+void configureBG0() {
+  // set the character base block and screenbase block of background 0.
+  // each background tile is 4bpp. background is of size 32x32 tiles.
+  REG_BG0CNT = BG_CBB(BG0_CBB) | BG_SBB(BG0_SBB) | BG_4BPP | BG_REG_32x32;
   // set scrolling registers to 0
   REG_BG0HOFS = 0;
   REG_BG0VOFS = 0;
+}
 
-  // load background data into memory
-  memcpy32(&tile_mem[CBB_INDEX][0], backgroundTiles,
+// initialize tonc text engine (tte) into bg 1
+void setupTTEBG1() {
+  tte_init_se(0,                      // BG 0
+              BG_CBB(0) | BG_SBB(31), // BG control (for REG_BGxCNT)
+              0,                      // Tile offset (special cattr)
+              CLR_YELLOW,             // Ink color
+              0,                      // BitUnpack offset (on-pixel = 15)
+              NULL,                   // Default font (sys8)
+              NULL);                  // Default renderer (se_drawg_s)
+  tte_write("\n Hello world! in yellow\n");
+}
+
+// load background data into memory
+void loadBGData() {
+  memcpy32(&tile_mem[BG0_CBB][0], backgroundTiles,
            backgroundTilesLen / sizeof(u32));
   memcpy16(pal_bg_mem, backgroundPal, backgroundPalLen / sizeof(u16));
+}
 
-  // load sprite data into memory at very first sprite charblock (4)
+// load sprite data into memory at very first sprite charblock (4)
+void loadSpriteData() {
   memcpy32(&tile_mem[4][0], spritesTiles, spritesTilesLen / sizeof(u32));
   memcpy16(pal_obj_mem, spritesPal, spritesPalLen / sizeof(u16));
 }
 
-// initializes floor tiles and non-dino sprites
+void load_world() {
+  // configureBG0();
+
+  setupTTEBG1();
+
+  // loadBGData();
+
+  // loadSpriteData();
+}
+
+// initializes floor tiles
 void init_world() {
   int scr_entry_start = FLOOR_SCR_ENTRY_Y * BG_DIM;
   int amt_bg_tiles = backgroundTilesLen / sizeof(u32) / TILE_32BIT_AMT;
   for (int i = scr_entry_start; i < BG_DIM + scr_entry_start; i++) {
     u16 tile_index = (i % (amt_bg_tiles - 1)) + 1;
-    se_mem[SBB_INDEX][i] = tile_index;
+    se_mem[BG0_SBB][i] = tile_index;
   }
 }
