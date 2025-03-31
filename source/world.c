2026ca84361a369c2bcfca37d9b7f57fe42d7735
Daniel Velasquez <danivelas4@gmail.com>
2025-03-30 20:29:56 -0400

    using multiple backgrounds M source/world.c

--- source/world.c
+++ source/world.c
@@ -15,6 +15,11 @@ static const int BG1_CBB = 0;
 static const int BG0_SBB = 31;
 static const int BG1_SBB = 30;
 
+// we'll use CBB #1 for generic background tiles in the deepest background (BG0)
+// and CBB #0 for text, one background above (BG1)
+static const int BG0_PALBANK = 1;
+static const int BG1_PALBANK = 0;
+
 void configureBG0() {
   // set the character base block and screenbase block of background 0.
   // each background tile is 4bpp. background is of size 32x32 tiles.
@@ -26,7 +31,7 @@ void configureBG0() {
 
 // initialize tonc text engine (tte) into bg 1
 void setupTTEBG1() {
-  tte_init_se(1,                                 // BG 0
+  tte_init_se(1,                                 // BG 1
               BG_CBB(BG1_CBB) | BG_SBB(BG1_SBB), // BG control (for REG_BGxCNT)
               0,                                 // Tile offset (special cattr)
               CLR_YELLOW,                        // Ink color
@@ -40,7 +45,7 @@ void setupTTEBG1() {
 void loadBGData() {
   memcpy32(&tile_mem[BG0_CBB][0], backgroundTiles,
            backgroundTilesLen / sizeof(u32));
-  memcpy16(pal_bg_mem, backgroundPal, backgroundPalLen / sizeof(u16));
+  memcpy16(pal_bg_bank[1], backgroundPal, backgroundPalLen / sizeof(u16));
 }
 
 // load sprite data into memory at very first sprite charblock (4)
@@ -54,17 +59,30 @@ void load_world() {
 
   setupTTEBG1();
 
-  // loadBGData();
+  loadBGData();
 
-  // loadSpriteData();
+  loadSpriteData();
+
+  init_world();
 }
 
 // initializes floor tiles
 void init_world() {
   int scr_entry_start = FLOOR_SCR_ENTRY_Y * BG_DIM;
   int amt_bg_tiles = backgroundTilesLen / sizeof(u32) / TILE_32BIT_AMT;
+
+  for (int i = 0; i < BG_DIM * BG_DIM; i++) {
+    // set floor tiles
+    if (i >= scr_entry_start && i < BG_DIM + scr_entry_start) {
+      u16 tile_index = (i % (amt_bg_tiles - 1)) + 1;
+      se_mem[BG0_SBB][i] = 0x1000 | tile_index;
+      continue;
+    }
+
+    // set background tiles
+    se_mem[BG0_SBB][i] = 0x1000;
+  }
+
   for (int i = scr_entry_start; i < BG_DIM + scr_entry_start; i++) {
-    u16 tile_index = (i % (amt_bg_tiles - 1)) + 1;
-    se_mem[BG0_SBB][i] = tile_index;
   }
 }
