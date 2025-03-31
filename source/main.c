560f64cbde83df76f33db3e5b59aaf904bb63002
Daniel Velasquez <danivelas4@gmail.com>
2025-03-31 01:41:40 -0400

    re-uncommented everything M source/main.c

--- source/main.c
+++ source/main.c
@@ -16,51 +16,49 @@ int main() {
 
   // load background and sprite assets
   load_world();
-  // also initialize the background on the screen
-  // init_world();
 
   // initialize our object buffer that will be used for the rest of this program
-  // struct buffer_state buffer_state = init_buffer();
-  //// initialize dino state with a pointer to an object buffer
-  // struct dino_state dino_state = init_dino_state(&buffer_state);
-  //// init cacti
-  // struct cactus_state *cacti_state = init_cacti_state(&buffer_state);
+  struct buffer_state buffer_state = init_buffer();
+  // initialize dino state with a pointer to an object buffer
+  struct dino_state dino_state = init_dino_state(&buffer_state);
+  // init cacti
+  struct cactus_state *cacti_state = init_cacti_state(&buffer_state);
 
-  // int frame = 0;
-  // int scroll_velocity = 2;
-  // int scroll_offset = 0;
-  // while (1) {
-  //   VBlankIntrWait();
-  //   key_poll();
+  int frame = 0;
+  int scroll_velocity = 2;
+  int scroll_offset = 0;
+  while (1) {
+    VBlankIntrWait();
+    key_poll();
 
-  //  // update OAM with new values that were calculated in last frame
-  //  oam_copy(oam_mem, buffer_state.obj_buffer, buffer_state.len);
-  //  // if out dinosaur crashed into something last iteration, lets break
-  //  if (!dino_state.alive) {
-  //    break;
-  //  }
+    // update OAM with new values that were calculated in last frame
+    oam_copy(oam_mem, buffer_state.obj_buffer, buffer_state.len);
+    // if out dinosaur crashed into something last iteration, lets break
+    if (!dino_state.alive) {
+      break;
+    }
 
-  //  // update dino state struct, and dino buffer
-  //  update_dino_state(&dino_state, frame);
+    // update dino state struct, and dino buffer
+    update_dino_state(&dino_state, frame);
 
-  //  // update cacti state struct, and cacti buffers
-  //  update_cacti_state(cacti_state, scroll_velocity);
+    // update cacti state struct, and cacti buffers
+    update_cacti_state(cacti_state, scroll_velocity);
 
-  //  // if collision happened, update dino state to be gameover
-  //  if (detected_collision(dino_state, cacti_state)) {
-  //    dino_state.alive = 0;
-  //    update_dino_state(&dino_state, frame);
-  //  }
+    // if collision happened, update dino state to be gameover
+    if (detected_collision(dino_state, cacti_state)) {
+      dino_state.alive = 0;
+      update_dino_state(&dino_state, frame);
+    }
 
-  //  // scroll horizontal window
-  //  scroll_offset += scroll_velocity;
-  //  REG_BG0HOFS = scroll_offset;
-  //  frame += 1;
-  //}
+    // scroll horizontal window
+    scroll_offset += scroll_velocity;
+    REG_BG1HOFS = scroll_offset;
+    frame += 1;
+  }
 
   // game over in low power mode
   while (1) {
-    // Halt();
+    Halt();
   }
 
   return 0;
