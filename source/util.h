8576326c472188cdab3b69dd1520d37ca558a625
Daniel Velasquez <danivelas4@gmail.com>
2025-04-19 23:37:46 -0400

    M source/util.h

--- source/util.h
+++ source/util.h
@@ -5,11 +5,7 @@
 #define SPRITE_TILE_AMT 16
 // one tile is 8x8 pixels
 #define TILE_PIXEL_DIM 8
-// one tile is 8 32-bit ints
-#define TILE_32BIT_AMT 8
 
-// lets use 32x32 tiles for our background
-#define BG_DIM 32
 // lets make floor 4 screenblock entries off ground
 #define FLOOR_SCR_ENTRY_Y (SCREEN_HEIGHT_T - 4)
 // lets make our sprites be 21 pixels above the floor
